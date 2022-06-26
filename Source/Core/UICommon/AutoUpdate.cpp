23929252bd2d82331577a955c57e4d9305720e35
LittleCoaks <LittleCoaks@users.noreply.github.com>
2022-06-26 00:01:02 -0400

    Linker error fix
    Unused static function :/

--- Source/Core/UICommon/AutoUpdate.cpp
+++ Source/Core/UICommon/AutoUpdate.cpp
@@ -135,24 +135,25 @@ bool AutoUpdateChecker::SystemSupportsAutoUpdates()
 #endif
 }
 
-static std::string GetPlatformID()
-{
-#if defined(_WIN32)
-#if defined(_M_ARM_64)
-  return "win-arm64";
-#else
-  return "win";
-#endif
-#elif defined(__APPLE__)
-#if defined(MACOS_UNIVERSAL_BUILD)
-  return "macos-universal";
-#else
-  return "macos";
-#endif
-#else
-  return "unknown";
-#endif
-}
+// Uncomment this when using it for the eventual use of auto-downloading/installing updates
+//static std::string GetPlatformID()
+//{
+//#if defined(_WIN32)
+//#if defined(_M_ARM_64)
+//  return "win-arm64";
+//#else
+//  return "win";
+//#endif
+//#elif defined(__APPLE__)
+//#if defined(MACOS_UNIVERSAL_BUILD)
+//  return "macos-universal";
+//#else
+//  return "macos";
+//#endif
+//#else
+//  return "unknown";
+//#endif
+//}
 
 void AutoUpdateChecker::CheckForUpdate(std::string_view update_track,
                                        std::string_view hash_override)
