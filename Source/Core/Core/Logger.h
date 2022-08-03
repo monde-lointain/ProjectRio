671dc25e6c820dc9034a6576486dd40ca88a765b
CNace13 <connornace13@gmail.com>
2022-08-03 12:51:15 -0400

    Switching logger to append mode

--- Source/Core/Core/Logger.h
+++ Source/Core/Core/Logger.h
@@ -13,18 +13,18 @@
 #include "Common/FileSearch.h"
 
 class Logger{
-    // Create Log file
-    std::string log_file_path = File::GetUserPath(D_STATELOGGER_IDX) + file_name + ".txt";
 
 public:
     // Constructor
     Logger(std::string in_file_name){
-        file_name = in_file_name;
+        log_file_path = File::GetUserPath(D_STATELOGGER_IDX) + in_file_name + ".txt";;
     };    
-    std::string file_name;
+    
+    // Path to log file
+    std::string log_file_path;
 
     void writeToFile(std::string in_string) {
-        File::WriteStringToFile(log_file_path, in_string + '\n');
+        File::WriteStringToFile(log_file_path, in_string + '\n', true);
     };
 
     void writeToTerminal(std::string in_string) {
