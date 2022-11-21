563b7774d53cba34ca3509e94ccbea9b1a5b3f1b
CNace13 <connornace13@gmail.com>
2022-11-21 16:19:43 -0500

    Replacing lylat with dolphin desktop

--- build-appimage.sh
+++ build-appimage.sh
@@ -46,6 +46,7 @@ mkdir -p AppDir
 ./Tools/linuxdeploy \
 	--appdir=./AppDir \
 	-e ./build/Binaries/dolphin-emu \
+	-d ./Data/dolphin-emu.desktop \
 	-i ./Data/RioLogo.png
 
 # Add the Sys dir to the AppDir for packaging
